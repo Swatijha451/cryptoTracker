1bb455c1d8fbe665453813349136135337901025
Swatijha451 <swatijha451@gmail.com>
2024-05-31 21:00:36 +0530

    Hopefully finished dedugging

--- src/components/Common/Button/Button.jsx
+++ src/components/Common/Button/Button.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import styles from "./Button.module.css";
 
-const Button =({ text, onClick, outlined })=>{
+const Button =({ text, outlined })=>{
     return(
          <div
       className={outlined ? `${styles.btnOutlined}` : `${styles.btn}`}
-      onClick={() => onClick()}
+      
     >
       {text}
     </div>
